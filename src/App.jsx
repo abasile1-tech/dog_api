33d461b468c691495ed009df56941b2047ba66a8
John <ashton.basile@gmail.com>
2023-08-23 11:43:12 +0100

    I refactored the code to use async await

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import "./App.css";
 function App() {
   const [dogImgUrl, setDogImgUrl] = useState("");
 
-  const fetchDog = () => {
-    console.log("Function called");
-    // fetch the JSON from URL
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((data) => setDogImgUrl(data.message));
-    // turn the JSON into JavaScript
-    // set the state of the dogImgUrl
+  // const fetchDog = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((data) => setDogImgUrl(data.message));
+  // };
+
+  const fetchDog = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    setDogImgUrl(data.message);
   };
 
   return (
