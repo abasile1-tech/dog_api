d0941eb15d118f07742d6b582a74a1339589528d
John <ashton.basile@gmail.com>
2023-08-23 11:40:09 +0100

    we fetch an image from the dog api each time the button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dogImgUrl, setDogImgUrl] = useState("");
+
+  const fetchDog = () => {
+    console.log("Function called");
+    // fetch the JSON from URL
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setDogImgUrl(data.message));
+    // turn the JSON into JavaScript
+    // set the state of the dogImgUrl
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="app">
+        <h1>RANDOM DOG</h1>
+        <img src={dogImgUrl} alt="dog" />
+        <button onClick={fetchDog}>Gimme a dog!</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
